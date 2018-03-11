be1badefcff5ee16099fd37406af01a5313c348d
Jian <jianqilu1126@gmail.com>
2018-03-11 22:16:37 +0000

    save

---
+++ script/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    let move_home_btn = function(){
+
+        $('#my_name').css({
+            'transform':'translateY(-500rem)',
+            'transition':'all 5s ease'
+        });
+
+        $('#quotes').css({
+            'opacity':0,
+            'transition':'all 1s ease'
+        });
+      
+        $('.view_all-container').css({
+            'transform':'translateY(500rem)',
+            'transition':'all 5s ease'
+        });
+        
+    }
+
+
+    $('.view_all-btn').click(()=>{
+        $('.gallery-container').css({
+            'transform':'translateX(-120%)' 
+        });
+
+        //move_home_btn();
+    })
+
+});
