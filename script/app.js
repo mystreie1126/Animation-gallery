cff81fe74c6f5870d502e92138736d67ec311665
Jian <jianqilu1126@gmail.com>
2018-03-12 01:22:03 +0000

    save

--- script/app.js
+++ script/app.js
@@ -18,13 +18,18 @@ $(document).ready(function(){
         
     }
 
-
     $('.view_all-btn').click(()=>{
         $('.gallery-container').css({
             'transform':'translateX(-120%)' 
         });
-
         move_home_btn();
     })
 
+    $('#contact').click(()=>{
+        $('.social-icon').css({
+            'opacity':1
+        });
+
+        console.log(123);
+    })
 });
